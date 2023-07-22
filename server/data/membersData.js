ace75dddeda3ff0f3a32813d30148dc7194bc96c
Slava <slava123101@gmail.com>
2023-07-22 16:53:47 +0500

    links fixed

--- server/data/membersData.js
+++ server/data/membersData.js
@@ -1,28 +1,33 @@
+const baseUrl =
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:3001'
+		: 'https://space-tourism-api.onrender.com';
+
 const crewData = [
 	{
 		name: 'Douglas Hurley',
-		link: `http://localhost:3000/crew-images/image-douglas-hurley.webp`,
+		link: `${baseUrl}/crew-images/image-douglas-hurley.webp`,
 		position: 'Commander',
 		description:
 			'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
 	},
 	{
 		name: 'MARK SHUTTLEWORTH',
-		link: `http://localhost:3000/crew-images/image-mark-shuttleworth.webp`,
+		link: `${baseUrl}/crew-images/image-mark-shuttleworth.webp`,
 		position: 'Mission Specialist',
 		description:
 			'Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
 	},
 	{
 		name: 'Victor Glover',
-		link: `http://localhost:3000/crew-images/image-victor-glover.webp`,
+		link: `${baseUrl}/crew-images/image-victor-glover.webp`,
 		position: 'PILOT',
 		description:
 			'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ',
 	},
 	{
 		name: 'Anousheh Ansari',
-		link: `http://localhost:3000/crew-images/image-anousheh-ansari.webp`,
+		link: `${baseUrl}/crew-images/image-anousheh-ansari.webp`,
 		position: 'Flight Engineer',
 		description:
 			'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space. ',
@@ -30,5 +35,5 @@ const crewData = [
 ];
 
 module.exports = {
-	crewData
-}
+	crewData,
+};
--- server/data/planetsData.js
+++ server/data/planetsData.js
@@ -1,3 +1,8 @@
+const baseUrl =
+	process.env.NODE_ENV !== 'production'
+		? 'http://localhost:3001'
+		: 'https://space-tourism-api.onrender.com';
+
 const planetsData = [
 	{
 		name: 'moon',
@@ -5,7 +10,7 @@ const planetsData = [
 			"See our planet as you've never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you're there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.",
 		distance: '384,400 km',
 		travelTime: '3 days',
-		link: `http://localhost:3000/planets-images/image-moon.webp`
+		link: `${baseUrl}/planets-images/image-moon.webp`
 	},
 	{
 		name: 'mars',
@@ -13,7 +18,7 @@ const planetsData = [
 			"Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It's two and a half times the size of Everest!",
 		distance: '225 MIL. km',
 		travelTime: '9 months',
-		link: `http://localhost:3000/planets-images/image-mars.webp`
+		link: `${baseUrl}/planets-images/image-mars.webp`
 	},
 	{
 		name: 'europa',
@@ -21,7 +26,7 @@ const planetsData = [
 			"The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover's dream. With an icy surface, it's perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.",
 		distance: '628 MIL. km',
 		travelTime: '3 years',
-		link: `http://localhost:3000/planets-images/image-europa.webp`
+		link: `${baseUrl}/planets-images/image-europa.webp`
 	},
 	{
 		name: 'titan',
@@ -29,7 +34,7 @@ const planetsData = [
 			'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
 		distance: '1.6 BIL. km',
 		travelTime: '7 years',
-		link: `http://localhost:3000/planets-images/image-titan.webp`
+		link: `${baseUrl}/planets-images/image-titan.webp`
 	},
 ];
 
